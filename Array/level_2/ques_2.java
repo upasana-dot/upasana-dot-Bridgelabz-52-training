acb24ea5d84aa84e3d15e326c1d59e24585451fd
Upasana Porwal <upasanaporwal8@gmail.com>
2025-09-23 15:25:07 +0530

    assignment_4

---
+++ Array/level_2/ques_2.java
@@ -0,0 +1,35 @@
+package level_2;
+
+import java.util.Scanner;
+
+public class ques_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            ages[i] = sc.nextInt();
+            System.out.print("Enter height of " + names[i] + " (in cm): ");
+            heights[i] = sc.nextDouble();
+        }
+        int youngestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (ages[i] < ages[youngestIndex]) {
+                youngestIndex = i;
+            }
+        }
+        int tallestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (heights[i] > heights[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+        System.out.println("Youngest friend is " + names[youngestIndex]);
+        System.out.println("Tallest friend is " + names[tallestIndex]);
+    }
+}
+
