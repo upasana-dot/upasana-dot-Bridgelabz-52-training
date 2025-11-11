d9ae5091dbe7de648b1ca0f46efb2745cc3c43ac
Upasana Porwal <upasanaporwal8@gmail.com>
2025-11-11 20:32:12 +0530

    Saved progress on Q13 before switching branch

--- Generics/Q13.java
+++ Generics/Q13.java
@@ -3,7 +3,6 @@ class Product<T> {
     String name;
     double price;
     T category;
-
     Product(String name, double price, T category) {
         this.name = name; this.price = price; this.category = category;
     }
