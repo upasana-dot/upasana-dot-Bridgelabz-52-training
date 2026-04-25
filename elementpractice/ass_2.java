391b3c61bdad4f7f780973952ef4307c2ba917ae
Upasana Porwal <upasanaporwal8@gmail.com>
2026-04-25 21:33:38 +0530

    Delete elementpractice directory
    unrelated file

--- elementpractice/ass_2.java
+++
@@ -1,9 +0,0 @@
-public class ass_2 {
-    public static void main(String[] args) {
-        int math=94;
-        int phy=95;
-        int chem=96;
-        System.err.println((math+phy+chem)/3);
-    }
-    
-}
--- elementpractice/ass_3.java
+++
@@ -1,8 +0,0 @@
-public class ass_3 {
-    public static void main(String[] args) {
-        double km=10.8;
-        System.err.println("the distance " +km+" km in miles is " + km*1.6 );
-
-    }
-    
-}
